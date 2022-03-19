01b16caa7e54799bae6ae8b6e2cafb37f923164f
Beni1228 <98697246+Beni1228@users.noreply.github.com>
2022-03-19 18:33:28 +0100

    ok

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-var ampferl = 4;
+var ampferl = 3;
 
 
 
 if(ampferl === 3){
-    document.getElementById('anzeige1').id = 'anzeige';
+    document.getElementById('Ampfer').id = 'anzeige';
+}
+if(ampferl === 1){
+    document.getElementById('Ampfer').id = 'none';
+}
+if(ampferl === 2){
+    document.getElementById('Ampfer').id = 'anzeige1';
+}
+
+
+var gräßerl = 2;
+
+
+
+if(gräßerl === 3){
+    document.getElementById('Gräßer').id = 'anzeige';
+}
+if(gräßerl === 1){
+    document.getElementById('Gräßer').id = 'none';
+}
+if(gräßerl === 2){
+    document.getElementById('Gräßer').id = 'anzeige1';
+}
+
+
+
+
+var pappell = 1;
+
+
+
+if(pappell === 3){
+    document.getElementById('Pappel').id = 'anzeige';
+}
+if(pappell === 1){
+    document.getElementById('Pappel').id = 'none';
+}
+if(pappell === 2){
+    document.getElementById('Pappel').id = 'anzeige1';
 }
